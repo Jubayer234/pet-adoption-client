9e9fc0b780210bf2a7802e91436309f6693960af
unknown <jubayerahmedtanvi@gmail.com>
2023-12-02 00:39:28 +0600

    update

--- src/Pages/Home/AnotherSection/AnotherSection.jsx
+++ src/Pages/Home/AnotherSection/AnotherSection.jsx
@@ -10,7 +10,7 @@ const AnotherSection = () => {
         
     }}>
     <div className=' text white p-16 mb-32 text-center'>
-            <h3 className='text-4xl text-white font bold font-serif pb-5'>Bistro Boss</h3>
+            <h3 className='text-4xl text-white font bold font-serif pb-5'>Adopt pet</h3>
             <p className='font-bold text-white'> people have the opportunity to make a positive impact on the lives of these creatures, offering them a safe haven and the chance to thrive in a nurturing environment. 
             </p>
         </div>
